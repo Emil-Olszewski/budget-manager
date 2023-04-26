7ef7dc3fb8a7b5b91455f0f1618e319e06a5aec2
Emil Olszewski <olszewski.emil.3712@gmail.com>
2023-04-26 15:07:02 +0200

    feat: polishing transaction edit

--- client/src/app/modules/home/components/edit-transaction/edit-transaction.component.ts
+++ client/src/app/modules/home/components/edit-transaction/edit-transaction.component.ts
@@ -1,23 +1,27 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { BehaviorSubject, combineLatestWith, forkJoin, mergeMap, Observable, take } from "rxjs";
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChildren } from '@angular/core';
+import { BehaviorSubject, combineLatestWith, forkJoin, mergeMap, Observable, Subject, take, takeUntil } from "rxjs";
 import { Transaction, TransactionType, UpdateTransaction } from "../../models/transaction";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { Account } from "../../models/account";
-import { Tag } from "../../models/tag";
+import { Account, Currency } from "../../models/account";
+import { Tag, TagType } from "../../models/tag";
+import { CurrencyCodeDirective } from "../../../../shared/directives/currency-code.directive";
 
 @Component({
   selector: 'app-edit-transaction',
   templateUrl: './edit-transaction.component.html',
   styleUrls: ['./edit-transaction.component.scss']
 })
-export class EditTransactionComponent implements OnInit {
+export class EditTransactionComponent implements OnInit, OnDestroy {
   private id!: number;
+  @ViewChildren(CurrencyCodeDirective) directives!: CurrencyCodeDirective[];
   @Input() public transaction$!: Observable<Transaction>;
   @Input() public accounts$!: Observable<Account[]>;
   @Input() public tags$!: Observable<Tag[]>
   @Output() public save$: EventEmitter<UpdateTransaction> = new EventEmitter<UpdateTransaction>();
   @Output() public delete$: EventEmitter<never> = new EventEmitter<never>();
   @Output() public goBack$: EventEmitter<never> = new EventEmitter<never>()
+  private accounts: Account[] = [];
+  private notifier$: Subject<never> = new Subject<never>();
   public transactionTags$: BehaviorSubject<Tag[]> = new BehaviorSubject<Tag[]>([]);
   public form: FormGroup = new FormGroup({
     account: new FormControl('', [Validators.required]),
@@ -26,9 +30,16 @@ export class EditTransactionComponent implements OnInit {
     date: new FormControl(new Date().toJSON().split('T')[0], [Validators.required]),
   });
 
+  public get AccountFromCurrency(): Currency {
+    return this.accounts.find(x => x.id === +this.form.controls['account'].value)?.currency ?? Currency.notDefined;
+  }
+
   public ngOnInit(): void {
+    this.accounts$.pipe(takeUntil(this.notifier$))
+      .subscribe(x => this.accounts = x);
+
     this.transaction$
-      .pipe(take(1), combineLatestWith(this.tags$))
+      .pipe(take(1), combineLatestWith(this.tags$, this.accounts$))
       .subscribe(x => {
         this.form.controls['account'].setValue(x[0].accountId);
         this.form.controls['name'].setValue(x[0].name);
@@ -36,7 +47,48 @@ export class EditTransactionComponent implements OnInit {
         this.form.controls['date'].setValue((new Date(x[0].date).toJSON().split('T')[0]));
         this.transactionTags$.next(x[1].filter(y => x[0].tags.map(z => z.id).includes(y.id)));
         this.id = x[0].id;
+
+        const account = x[2].find(account => account.id === x[0].accountId)!.currency;
+        // though timeout value is 0, it helps to properly initialize the directive
+        setTimeout(() => {
+          this.directives.forEach(x => {
+            x.initWithCurrency(account)
+          });
+        });
+     });
+
+    /*
+    every tag can have children, and every of them can have children too and so on
+    we need to get all of them and add to new array called 'flattenedTags'
+     */
+
+    this.tags$.pipe(takeUntil(this.notifier$))
+      .subscribe(x => {
+        const flattenedTags: Tag[] = [];
+        const getChildren = (tags: Tag[]) => {
+          tags.forEach(tag => {
+            flattenedTags.push(tag);
+            if (tag.children.length > 0) {
+              getChildren(tag.children);
+            }
+          });
+        };
+        getChildren(x);
+        this.tags$ = new BehaviorSubject<Tag[]>(flattenedTags);
       });
+
+    this.form.controls['account'].valueChanges
+      .pipe(takeUntil(this.notifier$))
+      .subscribe(x => {
+        this.directives.forEach(directive => {
+          directive.currencyChanged(this.accounts.find(account => account.id === +x)!.currency);
+        });
+      })
+  }
+
+  public ngOnDestroy() {
+    this.notifier$.next(null as never);
+    this.notifier$.complete();
   }
 
   public submit(): void {
@@ -65,4 +117,6 @@ export class EditTransactionComponent implements OnInit {
     }
       this.transactionTags$.next(selectedTags);
   }
+
+  protected readonly TagType = TagType;
 }
